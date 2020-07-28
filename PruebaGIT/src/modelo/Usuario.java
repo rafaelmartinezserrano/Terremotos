547e51424079c1fa2a35dcd5655eb025cb703dbb
profesor <profesor@DELL980-0.postal6.academiapostal.es.local>
2020-07-28 16:08:11 +0200

    Más cambios

--- PruebaGIT/src/modelo/Usuario.java
+++ PruebaGIT/src/modelo/Usuario.java
@@ -3,6 +3,13 @@ package modelo;
 public class Usuario {
 
 	private String login;
+	private String clave;
+	
+	public Usuario(String login, String clave) {
+		super();
+		this.login = login;
+		this.clave = clave;
+	}
 
 	public String getLogin() {
 		return login;
